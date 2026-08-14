b071170ba78c3a46b3a96f8b1dd2ce02b05217a5
harshith <harshithmadupu08@gmail.com>
2026-08-14 16:51:45 +0530

    second Commmit

--- index.js
+++ index.js
@@ -31,14 +31,14 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 
 
-const PORT = 4000;
+const PORT =process.env.PORT || 4000;
 
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-app.use('/home' , (req, res) => {
+app.use('/' , (req, res) => {
   res.send("Welcome to the home page");
 });
 
